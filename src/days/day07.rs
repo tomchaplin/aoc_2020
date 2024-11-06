bffb070ff0da18f97cb7b2501892ede292d28a91
Thomas Chaplin <tomrchaplin@gmail.com>
2024-11-06 15:44:09 +0000

    Day 7 + 8

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -8,26 +8,9 @@ pub struct Solution {}
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct BagType(String, String);
 
-#[derive(Debug)]
-struct BagLine {
-    main: BagType,
-    contents: Vec<(usize, BagType)>,
-}
-
 struct BagTree(HashMap<BagType, Vec<(usize, BagType)>>);
 
-impl From<Vec<BagLine>> for BagTree {
-    fn from(value: Vec<BagLine>) -> Self {
-        BagTree(
-            value
-                .into_iter()
-                .map(|line| (line.main, line.contents))
-                .collect(),
-        )
-    }
-}
-
-fn parse_input(input: &str) -> Vec<BagLine> {
+fn parse_input(input: &str) -> BagTree {
     let bag_type = parser!(
         word1:string(lower+) " " word2:string(lower+)
         => BagType(word1, word2)
@@ -47,14 +30,14 @@ fn parse_input(input: &str) -> Vec<BagLine> {
 
     let line_parser = parser!(
         main:bag_type " " bag_or_bags " contain "
-        contents:all_content => BagLine{main, contents}
+        contents:all_content => (main, contents)
     );
 
-    let p = parser!(lines(line_parser));
-    p.parse(input).unwrap()
+    let p = parser!(hash_map(lines(line_parser)));
+    BagTree(p.parse(input).unwrap())
 }
 
-fn eventually_contains(
+fn contains_shiny_gold(
     query_key: &BagType,
     bag_tree: &BagTree,
     cache: &mut HashMap<BagType, bool>,
@@ -64,14 +47,16 @@ fn eventually_contains(
         return *contains;
     }
 
+    let shiny_gold = BagType("shiny".to_string(), "gold".to_string());
+
     for (_count, bag_type) in bag_tree.0.get(&query_key).unwrap() {
         // If bag type is what we want then insert true
-        if *bag_type == BagType("shiny".to_string(), "gold".to_string()) {
+        if *bag_type == shiny_gold {
             cache.insert(query_key.clone(), true);
             return true;
         }
         // If any contents contains then we do
-        if eventually_contains(bag_type, bag_tree, cache) {
+        if contains_shiny_gold(bag_type, bag_tree, cache) {
             cache.insert(query_key.clone(), true);
             return true;
         }
@@ -112,7 +97,7 @@ impl ProblemSolution for Solution {
 
         let n_types_contain = all_types
             .iter()
-            .filter(|bag_type| eventually_contains(bag_type, &bag_tree, &mut cache))
+            .filter(|bag_type| contains_shiny_gold(bag_type, &bag_tree, &mut cache))
             .count();
 
         Some(n_types_contain.to_string())
@@ -120,9 +105,9 @@ impl ProblemSolution for Solution {
 
     fn solve_b(&self, input: &str) -> Option<String> {
         let bag_tree: BagTree = parse_input(input).into();
-        let query_key = BagType("shiny".to_string(), "gold".to_string());
+        let shiny_gold = BagType("shiny".to_string(), "gold".to_string());
         let mut cache = HashMap::default();
-        let contents = bags_inside(&query_key, &bag_tree, &mut cache);
+        let contents = bags_inside(&shiny_gold, &bag_tree, &mut cache);
         Some(contents.to_string())
     }
 }
--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,6 +1,117 @@
+use std::collections::HashSet;
+
 use crate::ProblemSolution;
 #[allow(unused_imports)]
 use aoc_parse::{parser, prelude::*};
 pub struct Solution {}
 
-impl ProblemSolution for Solution {}
+#[derive(Clone, Copy)]
+enum Instruction {
+    Acc(isize),
+    Jmp(isize),
+    Nop(isize),
+}
+use Instruction::*;
+
+impl Instruction {
+    fn swap(self) -> Self {
+        match self {
+            Acc(val) => Acc(val),
+            Jmp(val) => Nop(val),
+            Nop(val) => Jmp(val),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct MachineState {
+    position: usize,
+    accumulator: isize,
+}
+
+impl MachineState {
+    fn init() -> Self {
+        MachineState {
+            position: 0,
+            accumulator: 0,
+        }
+    }
+
+    fn update(&self, instruction: Instruction) -> Self {
+        use Instruction::*;
+        match instruction {
+            Acc(arg) => MachineState {
+                position: self.position + 1,
+                accumulator: self.accumulator + arg,
+            },
+            Jmp(arg) => MachineState {
+                position: (self.position as isize + arg) as usize,
+                accumulator: self.accumulator,
+            },
+            Nop(_) => MachineState {
+                position: self.position + 1,
+                accumulator: self.accumulator,
+            },
+        }
+    }
+}
+
+enum SimulationOutcome {
+    InfiniteLoop(isize),
+    Terminates(isize),
+}
+use SimulationOutcome::*;
+
+fn simulate(instructions: &Vec<Instruction>) -> SimulationOutcome {
+    let mut state = MachineState::init();
+    let mut prior_positions: HashSet<usize> = HashSet::default();
+    loop {
+        let next_instruction = instructions[state.position];
+        let next_state = state.update(next_instruction);
+        if prior_positions.contains(&next_state.position) {
+            // Return accumulator before loop begins
+            return InfiniteLoop(state.accumulator);
+        } else if next_state.position == instructions.len() {
+            // Return accumulator after final instruction runs
+            return Terminates(next_state.accumulator);
+        } else {
+            state = next_state;
+            prior_positions.insert(state.position);
+        }
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Instruction> {
+    let p = parser!(lines({
+        "acc " arg:isize => Acc(arg),
+        "jmp " arg:isize => Jmp(arg),
+        "nop " arg:isize => Nop(arg)
+    }));
+    p.parse(input).unwrap()
+}
+
+impl ProblemSolution for Solution {
+    fn solve_a(&self, input: &str) -> Option<String> {
+        let instructions = parse_input(input);
+        match simulate(&instructions) {
+            InfiniteLoop(acc) => Some(acc.to_string()),
+            Terminates(_) => unreachable!(),
+        }
+    }
+
+    fn solve_b(&self, input: &str) -> Option<String> {
+        let mut instructions = parse_input(input);
+        for idx in 0..instructions.len() {
+            instructions[idx] = instructions[idx].swap();
+            match simulate(&instructions) {
+                InfiniteLoop(_) => {
+                    // Undo instruction swap before continuing
+                    instructions[idx] = instructions[idx].swap();
+                    continue;
+                }
+                Terminates(acc) => return Some(acc.to_string()),
+            }
+        }
+        unreachable!()
+    }
+}
